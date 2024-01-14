efa5f49242f9a7bbc728deb410b31e2ee59cbdb7
EmmaCan <156542345+EmmaCan@users.noreply.github.com>
2024-01-14 10:02:17 -0500

    Update functions.h

--- functions.h
+++ functions.h
@@ -1,17 +1,11 @@
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
-// Do know change anything in this file. You will not submit this file
-// Here we included the declaration of all functions you need
-
 
 // This is the Objective function which the implementation is in OF.c
 double Objective_function(int NUM_VARIABLES, double x[NUM_VARIABLES]);
 
 // This function generate a random number with precision double between "min" and "max"
 double generate_random(double min, double max);
-
-// This function generates a random integer value
-unsigned int generate_int();
 
 // Initializing the population randomly between the the lower and upper bound
 void generate_population(int POPULATION_SIZE, int NUM_VARIABLES, double population[POPULATION_SIZE][NUM_VARIABLES], double Lbound[NUM_VARIABLES], double Ubound[NUM_VARIABLES]);
